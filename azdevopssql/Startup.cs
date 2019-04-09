17f5705cb78ddf6be56975835cea7b8f760f953d
Eduard Ekob <eduard.ekob@gmail.com>
2019-04-09 05:17:12 +0300

    commit7

--- azdevopssql/Startup.cs
+++ azdevopssql/Startup.cs
@@ -45,7 +45,7 @@ namespace Games
                 c.SwaggerDoc("v1", new Info
                 {
                     Version = "v1",
-                    Title = "ToDo API Staging and Deploy from Git to Azure",
+                    Title = "ToDo API Staging and Deploy from Git to Azure Trigger",
                     Description = "A simple example ASP.NET Core Web API",
                     TermsOfService = "None",                    
                 });
